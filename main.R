3407cc77e4f7b7e7f35f2c9e48267c2be9e00194
hailmarvin <mmwanzi76@gmail.com>
2021-03-13 14:42:59 +0300

    lecture two

--- main.R
+++ main.R
@@ -27,3 +27,53 @@ plot(ts_model, 3)
 library(car)
 
 durbinWatsonTest(ts_model)
+
+###################
+# Air Passengers Data-set
+
+# Load Packages
+library(ggfortify)
+library(tseries)
+library(forecast)
+
+# Load Data
+data("AirPassengers")
+AP <- AirPassengers
+
+class(AP)
+
+# AP <- ts(AP) - If data-set is not of class "ts"
+
+#############################
+# Part 1
+# Perform Exploratory Data analysis
+AP
+
+# Check for missing values
+sum(is.na(AP))
+
+# Check Frequency of the time series
+frequency(AP)
+
+# Check cycle of the Time series
+cycle(AP)
+
+# Review the table summary
+summary(AP)
+
+# Plot the raw data using the base plot function
+plot(AP,xlab="Date", ylab = "Passenger numbers (1000's)",main="Air Passenger numbers from 1949 to 1961")
+
+# Alternative plotting function from "ggfortify" package
+autoplot(AP) + labs(x ="Date", y = "Passenger numbers (1000's)", title="Air Passengers from 1949 to 1961")
+
+# Using boxplot function to see Seasonal Effects
+boxplot(AP~cycle(AP),xlab="Date", ylab = "Passenger Numbers (1000's)" ,main ="Monthly Air Passengers Boxplot from 1949 to 1961")
+
+# Part II
+# Time Series Decomposition
+
+# We saw an indication of a multiplicative model
+
+decomposeAP <- decompose(AP, "multiplicative")
+autoplot(decomposeAP)
